83ff9bedc6b102607f2c503a0d2de9998cf3f1aa
Eric Zhou <ironduckbill@gmail.com>
2023-03-21 12:30:40 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,40 +7,147 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
     public float speed;
-    public Text score;
-    private int scoreValue = 0;
+    private bool facingRight = true;
+    [SerializeField] private bool isOnGround;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask allGround;
+    public Text score, lives;
+    public GameObject winText, loseText;
+    public int scoreValue = 0, livesValue = 3;
+    public AudioClip musicClipOne, musicClipTwo, musicClipThree;
+    public AudioSource musicSource;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        SetScore();
+        SetLives();
+        winText.SetActive(false);
+        loseText.SetActive(false);
+        PlayMusic();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
-        float verMovement = Input.GetAxis("Vertical");
 
-        rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+        rd2d.AddForce(new Vector2(hozMovement * speed, 0));
+        isOnGround = Physics2D.OverlapCircle(groundCheck.position, checkRadius, allGround);
+
+        if(isOnGround)
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 4), ForceMode2D.Impulse);
+            }
+        }
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+    }
+    void Update()
+    {
+        if(isOnGround)
+        {
+            if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            {
+                anim.SetInteger("State", 1);
+            }
+            else if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            {
+                anim.SetInteger("State", 0);
+            }
+            else
+            {
+                anim.SetInteger("State", 0);
+            }
+        }
+        if(!isOnGround)
+        {
+            anim.SetInteger("State", 2);
+        }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            SetScore();
+            Destroy(collision.collider.gameObject);
+        }
+        if(collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            SetLives();
             Destroy(collision.collider.gameObject);
         }
     }
-    private void OnCollisionStay2D(Collision2D collision)
+    void Flip()
     {
-        if(collision.collider.tag == "Ground")
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+    void SetScore()
+    {
+        score.text = "Score: " + scoreValue.ToString();
+
+        if(scoreValue == 4)
         {
-            if(Input.GetKey(KeyCode.W))
-            {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
-            }
+            transform.position = new Vector2(31.0f, 1.0f);
+            livesValue = 3;
+            SetLives();
+            PlayMusic();
+        }
+        if(scoreValue == 8)
+        {
+            winText.SetActive(true);
+            PlayMusic();
+        }
+    }
+    void SetLives()
+    {
+        lives.text = "Lives: " + livesValue.ToString();
+
+        if(livesValue <= 0)
+        {
+            winText.SetActive(false);
+            loseText.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+    void PlayMusic()
+    {
+        if(scoreValue < 4)
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+        if(scoreValue == 4)
+        {
+            musicSource.Stop();
+            musicSource.clip = musicClipTwo;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+        if(scoreValue == 8)
+        {
+            musicSource.Stop();
+            musicSource.clip = musicClipThree;
+            musicSource.loop = false;
+            musicSource.Play();
         }
     }
 }
